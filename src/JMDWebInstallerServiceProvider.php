f32432d6c854fb7350bf3c1873bb815b5a739e75
Pappu Singh <pappu@jmdweb.nl>
2022-08-25 16:26:45 +0530

    first commit

---
+++ src/JMDWebInstallerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Jmdweb\Installer;
+
+use Illuminate\Support\ServiceProvider;
+
+class JMDWebInstallerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+}
