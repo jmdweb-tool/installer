ade7061fb82a6ad12e073b505374e1d136aac8c7
Pappu Singh <pappu@jmdweb.nl>
2022-08-29 19:59:36 +0530

    Webpack mix checked for replacement

--- src/Console/Commands/CopyWebpackMix.php
+++ src/Console/Commands/CopyWebpackMix.php
@@ -15,8 +15,11 @@ trait CopyWebpackMix
         }
 
         $update_mix = file_get_contents(base_path('Jmdweb/stubs/webpack.mix.js'));
+        $check_if_already_have_the_module_script = file_get_contents(base_path('webpack.mix.js'));
 
-        if(!Str::contains( $update_mix,"Jmdweb/views/vue/app.js")) {
+       
+
+        if(!Str::contains( $check_if_already_have_the_module_script,"Jmdweb/views/vue/app.js")) {
             file_put_contents(base_path('webpack.mix.js'), $update_mix.PHP_EOL , FILE_APPEND | LOCK_EX);
         }
 
